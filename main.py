574f498f64ac19a45a2385f59e10462aba7f199f
a-powers <awpowers@gmail.com>
2021-02-12 16:10:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+names_string = input("Give me everybody's names, separated by a comma. ")
+#create a list of names
+names = names_string.split(", ")
+
+import random
+
+random_position = random.randrange(0, len(names))
+picked_name = names[random_position]
+
+print(f"The person who pays for the meal is {picked_name}.")
